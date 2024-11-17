5edf60c9c5ec2d09f9cc8ec52f1a6343d9124fe2
hemantsoni23 <sonih5190@gmail.com>
2024-11-17 18:25:22 +0530

    commit

--- frontend/src/App/Store.jsx
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "../redux/AuthSlice";
-
-export const Store = configureStore({
-    reducer: {
-        auth: authReducer
-    }
-});
